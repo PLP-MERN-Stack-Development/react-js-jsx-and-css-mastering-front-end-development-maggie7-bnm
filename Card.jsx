d49ec0937d344199d35729d84b252ba7c54379f4
Bevtheedev <mugobev@gmail.com>
2025-10-27 13:35:44 +0300

    Delete Card.jsx

--- Card.jsx
+++
@@ -1,9 +0,0 @@
-const Card = ({ children, className = "" }) => {
-  return (
-    <div className={`bg-white dark:bg-gray-800 rounded-lg shadow-md p-6 ${className}`}>
-      {children}
-    </div>
-  )
-}
-
-export default Card
